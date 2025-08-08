40befd1387cf63e26197b5bcf9a35e34ffbe8576
tommy-u <tunger10@gmail.com>
2025-08-08 11:51:03 -0700

    Remove mitosisctl

--- scheds/rust/scx_mitosis/mitosisctl/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    scx_utils::BpfBuilder::new()
-        .unwrap()
-        .enable_intf("../src/bpf/intf.h", "bpf_intf.rs")
-        .enable_skel("../src/bpf/mitosis.bpf.c", "bpf")
-        .build()
-        .unwrap();
-}
--- scheds/rust/scx_mitosis/mitosisctl/src/bpf_skel.rs
+++
@@ -1,1 +0,0 @@
-include!(concat!(env!("OUT_DIR"), "/bpf_skel.rs"));
